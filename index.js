f7755a480c66fd4936b7291899731bfc4aa162be
Jesse Williamson <jesseowilliamson@gmail.com>
2021-05-19 10:20:58 -0600

    added funtionality to create new user and to login to already created  users

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const app = express();
 const fs = require('fs');
 const cors = require('cors');
 const port = process.env.PORT || 3001;
-
-app.get('blog-data.json', (req, res) => {
-    res.status(200).json({ api: 'version 1' })
-})
-
-
 var blogData = JSON.parse(fs.readFileSync('blog-data.json'));
 
 app.use(cors());
@@ -16,26 +10,55 @@ app.use(express.json());
 
 app.get('/blog-data', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(blogData));
+    res.send(JSON.stringify(blogData))
+})
+
+app.get('/blog-data/:username', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(blogData[req.params.username].blogs))
+})
+
+app.put('/blog-data', function (req, res) {
+    if (req.body.username && req.body.password && !blogData[req.body.username]) {
+
+        blogData[req.body.username] = { password: req.body.password, blogs: [] }
+        fs.writeFileSync('blog-data.json', JSON.stringify(blogData))
+        res.setHeader('Content-Type', 'application/json')
+        res.send({})
+    } else return res.status(400).send('mission either username or password for new user')
+
 });
 
-app.post('/blog-data', function (req, res) {
+app.post('/blog-data/:username', function (req, res) {
+    if (blogData[req.params.username] && req.body && blogData[req.params.username].password === req.body.password) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(blogData[req.params.username].blogs));
+    } else return res.status(401).send('password does not match or username does not exist')
+});
+
+app.patch('/blog-data/:username', function (req, res) {
     if (!req.body) {
         res.send(JSON.stringify(blogData));
         return;
     }
-    blogData.blogs.push({ ...req.body, id: (blogData.blogs[blogData.blogs.length - 1].id + 1) });
+
+    if (blogData[req.params.username].blogs.length < 1) {
+        var blogId = 0
+    } else {
+        var blogId = (blogData[req.params.username].blogs[blogData[req.params.username].blogs.length - 1].id + 1)
+    }
+    blogData[req.params.username].blogs.push({ ...req.body, id: blogId });
     fs.writeFileSync('blog-data.json', JSON.stringify(blogData));
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(blogData));
 });
 
-app.delete(`/blog-data/:blogId`, function (req, res) {
+app.delete(`/blog-data/:username/:blogId`, function (req, res) {
     const blogId = req.params.blogId;
-    blogData.blogs = blogData.blogs.filter(blog => blog.id != blogId)
+    blogData[req.params.username].blogs = blogData[req.params.username].blogs.filter(blog => blog.id != blogId)
     fs.writeFileSync('blog-data.json', JSON.stringify(blogData));
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(blogData))
+    res.send(JSON.stringify(blogData[req.params.username].blogs))
 })
 
 app.listen(port, () => console.log('server started on port', port));
